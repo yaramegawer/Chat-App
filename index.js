f7df116bf51e8fb86cdf99095ad95933fa971d83
yaramegawer <108768001+yaramegawer@users.noreply.github.com>
2024-03-03 23:18:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const io=new Server(server);
 app.use(express.static('./public'))
 
 const port=process.env.PORT || 5000
-const ip = "192.168.1.4";
+const ip = "YOUR_IP_ADDRESS";
 app.get('/', (req, res) => {
 
     res.sendFile(__dirname + '/public/index.html');
